baa026161322c23926b3353515fb8b324cb47d6a
cwd3469 <cwd3469@naver.com>
2022-07-21 10:15:42 +0900

    배포 오류 수정

--- src/components/career/CareerSide.tsx
+++ src/components/career/CareerSide.tsx
@@ -11,65 +11,65 @@ const CareerSide = () => {
         <Swiper direction={"vertical"} slidesPerView={6} spaceBetween={0} mousewheel={true} modules={[Mousewheel]} className="career-swiper">
           <SwiperSlide>
             <CareerBox title="타입라인">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
           <SwiperSlide>
             {" "}
             <CareerBox title="업무경험">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
 
           <SwiperSlide>
             {" "}
             <CareerBox title="학력">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
 
           <SwiperSlide>
             {" "}
             <CareerBox title="개인 프로젝트">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
 
           <SwiperSlide>
             <CareerBox title="수상/자격증">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
 
           <SwiperSlide>
             {" "}
             <CareerBox title="출판/논문/특허">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
 
           <SwiperSlide>
             <CareerBox title="테스트/과제관 결과">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
 
           <SwiperSlide>
             <CareerBox title="활동">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
 
           <SwiperSlide>
             <CareerBox title="외국어">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
 
           <SwiperSlide>
             {" "}
             <CareerBox title="첨부 파일">
-              <Box bg="#333" w="100%" h="100%"></Box>
+              <Box w="100%" h="100%"></Box>
             </CareerBox>
           </SwiperSlide>
         </Swiper>
