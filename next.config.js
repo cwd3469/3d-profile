4198712fbf22c476bebb08541a0c114a2219529d
cwd3469 <cwd3469@naver.com>
2022-07-21 11:54:29 +0900

    배포 오류 수정

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     return {
       "/": { page: "/" },
       "/todo": { page: "/todo" },
-      "/carrer": { page: "/carrer" },
+      "/career": { page: "/career" },
     };
   },
   webpack(config, { webpack }) {
